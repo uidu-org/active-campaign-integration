aaae5229a65bae2bf36f2299676da931f69fb941
Andrea Vanini <andrea.vanini@uidu.org>
2020-09-16 18:45:04 +0200

    Bump dependencies

--- lib/active_campaign_integration/jobs/sync_job.rb
+++ lib/active_campaign_integration/jobs/sync_job.rb
@@ -6,6 +6,7 @@ module ActiveCampaignIntegration
       queue_as ActiveCampaignIntegration.queue_name
 
       discard_on ActiveJob::DeserializationError
+      discard_on Aws::SQS::Errors::InvalidParameterValue
 
       def perform(user, _timestamp = Time.now.to_i)
         # Do something later
