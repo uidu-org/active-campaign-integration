fac0358b7e1eff095aea60cbeca45d3848cafef9
Andrea Vanini <andrea.vanini@uidu.org>
2020-10-01 10:37:11 +0200

    Do not use aws if not present

--- lib/active_campaign_integration/jobs/sync_job.rb
+++ lib/active_campaign_integration/jobs/sync_job.rb
@@ -6,7 +6,7 @@ module ActiveCampaignIntegration
       queue_as ActiveCampaignIntegration.queue_name
 
       discard_on ActiveJob::DeserializationError
-      discard_on Aws::SQS::Errors::InvalidParameterValue
+      # discard_on ::Aws::SQS::Errors::InvalidParameterValue
 
       def perform(user, _timestamp = Time.now.to_i)
         # Do something later
